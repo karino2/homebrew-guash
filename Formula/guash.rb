11d967285c1e05c77e821eb5845904ae8807f283
Kazuma Arino <hogeika2@gmail.com>
2022-01-28 17:37:56 +0900

    bump guash version to 1.0.1

--- Formula/guash.rb
+++ Formula/guash.rb
@@ -1,8 +1,8 @@
 class Guash < Formula
   desc "GUI app scripting by shell script."
   homepage "https://github.com/karino2/guash/"
-  url "https://github.com/karino2/guash/releases/download/v1.0.0/guash-darwin-x64-1.0.0.zip"
-  sha256 "e3d4ea2998f65a210a288caf2614ec51b1d692c40ffbf2833cf07500dc4e895a"
+  url "https://github.com/karino2/guash/releases/download/v1.0.1/guash.1.0.1.zip"
+  sha256 "dc53478df55bb8728420bebd40cf5fc57bbf48c171f53d7865d4954e83734e3f"
   license "MIT"
 
   depends_on "dotnet"
